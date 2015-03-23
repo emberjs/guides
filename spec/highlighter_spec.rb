d0fb3b86481a318d4a2b3448333f5f04e7e2b281
Dan McClain <git@danmcclain.net>
2015-03-22 21:29:38 -0400

    Implements highlighting
    At the end of the filename or language add `{1,2,-4,+5}`,
    `+` signifies that the line should be marked as added
    `-` signifies that the line should be marked as removed
    line numbers are otherwise highlighted
    
    Does not currently handle ranges
    
    Also updates colors in code blocks, fixing contrast

--- spec/highlighter_spec.rb
+++ spec/highlighter_spec.rb
@@ -12,37 +12,65 @@ describe Highlighter::Helpers do
     Object.send :remove_const, :HelperTester
   end
 
-  describe '#_detect_language_and_filename' do
+  describe '#_detect_language_filename_and_changes' do
     it 'returns bare languages and does not return a filename' do
-      language, filename = HelperTester.new._detect_language_and_filename('javascript')
+      language, filename, changes = HelperTester.new._detect_language_filename_and_changes('javascript')
       expect(language).to eq 'javascript'
       expect(filename).to be_nil
+      expect(changes).to eq []
 
-      language, filename = HelperTester.new._detect_language_and_filename('handlebars')
+      language, filename, changes = HelperTester.new._detect_language_filename_and_changes('handlebars')
       expect(language).to eq 'handlebars'
       expect(filename).to be_nil
+      expect(changes).to eq []
     end
 
     it 'returns the detected language and the filename from a filename' do
-      language, filename = HelperTester.new._detect_language_and_filename('app/components/my-component.js')
+      language, filename, changes = HelperTester.new._detect_language_filename_and_changes('app/components/my-component.js')
       expect(language).to eq 'javascript'
       expect(filename).to eq 'app/components/my-component.js'
+      expect(changes).to eq []
 
-      language, filename = HelperTester.new._detect_language_and_filename('app/templates/application.hbs')
+      language, filename, changes = HelperTester.new._detect_language_filename_and_changes('app/templates/application.hbs')
       expect(language).to eq 'handlebars'
       expect(filename).to eq 'app/templates/application.hbs'
+      expect(changes).to eq []
 
-      language, filename = HelperTester.new._detect_language_and_filename('app/index.html')
+      language, filename, changes = HelperTester.new._detect_language_filename_and_changes('app/index.html')
       expect(language).to eq 'html'
       expect(filename).to eq 'app/index.html'
+      expect(changes).to eq []
 
-      language, filename = HelperTester.new._detect_language_and_filename('app/styles/app.css')
+      language, filename, changes = HelperTester.new._detect_language_filename_and_changes('app/styles/app.css')
       expect(language).to eq 'css'
       expect(filename).to eq 'app/styles/app.css'
+      expect(changes).to eq []
 
-      language, filename = HelperTester.new._detect_language_and_filename('bower.json')
+      language, filename, changes = HelperTester.new._detect_language_filename_and_changes('bower.json')
       expect(language).to eq 'json'
       expect(filename).to eq 'bower.json'
+      expect(changes).to eq []
+    end
+
+    it 'returns detected code changes' do
+      _, _, changes = HelperTester.new._detect_language_filename_and_changes('app/components/my-component.js{1,+3,4,-5,+6,-7}')
+
+      expect(changes).to eq [
+        [1, nil],
+        [3, 'added'],
+        [4, nil],
+        [5, 'removed'],
+        [6, 'added'],
+        [7, 'removed'],
+      ]
+
+      _, _, changes = HelperTester.new._detect_language_filename_and_changes('javascript{+2,3,-5}')
+
+      expect(changes).to eq [
+        [2, 'added'],
+        [3, nil],
+        [5, 'removed']
+      ]
     end
   end
 
@@ -81,6 +109,48 @@ OUTPUT
 </pre></td>
   <td class="code"><pre><span class="reserved">export</span> <span class="keyword">default</span> Ember.Component.extend()</pre></td>
 </tr></table>
+</div></div>
+OUTPUT
+    end
+
+    it 'returns a code block with a filename in the table when using a filename fence with highlighting' do
+      code_block = HelperTester.new._highlight("export default Ember.Component.extend()\nvar added;\nvar removed;",
+                                               'app/components/my-foo.js{1,+2,-3}')
+      expect(code_block).to eq <<-OUTPUT.sub(/\n$/, '')
+<div class="highlight javascript "><div class="ribbon"></div><div class="scroller"><?xml version="1.0"?>
+<table class="CodeRay">
+  <thead>
+    <tr>
+      <td colspan="2">app/components/my-foo.js</td>
+    </tr>
+  </thead>
+<tr>
+  <td class="line-numbers"><pre><span class="highlight-line ">1</span>
+<span class="highlight-line added">2</span>
+<span class="highlight-line removed">3</span></pre></td>
+  <td class="code"><pre><span class="highlight-line "><span class="reserved">export</span> <span class="keyword">default</span> Ember.Component.extend()</span>
+<span class="highlight-line added"><span class="keyword">var</span> added;</span>
+<span class="highlight-line removed"><span class="keyword">var</span> removed;</span></pre></td>
+</tr></table>
+</div></div>
+OUTPUT
+    end
+
+    it 'returns a code block with a filename in the table when using a filename fence with highlighting' do
+      code_block = HelperTester.new._highlight("export default Ember.Component.extend()\nvar added;\nvar removed;",
+                                               'javascript{1,+2,-3}')
+      expect(code_block).to eq <<-OUTPUT.sub(/\n$/, '')
+<div class="highlight javascript "><div class="ribbon"></div><div class="scroller"><?xml version="1.0"?>
+<table class="CodeRay">
+  <tr>
+  <td class="line-numbers"><pre><span class="highlight-line ">1</span>
+<span class="highlight-line added">2</span>
+<span class="highlight-line removed">3</span></pre></td>
+  <td class="code"><pre><span class="highlight-line "><span class="reserved">export</span> <span class="keyword">default</span> Ember.Component.extend()</span>
+<span class="highlight-line added"><span class="keyword">var</span> added;</span>
+<span class="highlight-line removed"><span class="keyword">var</span> removed;</span></pre></td>
+</tr>
+</table>
 </div></div>
 OUTPUT
     end
