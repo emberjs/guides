246d188d401c9f24a03060e64b3eacbd4225a6cc
Dan McClain <git@danmcclain.net>
2015-03-22 21:29:38 -0400

    Implements highlighting
    At the end of the filename or language add `{1,2,-4,+5}`,
    `+` signifies that the line should be marked as added
    `-` signifies that the line should be marked as removed
    line numbers are otherwise highlighted
    
    Does not currently handle ranges
    
    Also updates colors in code blocks, fixing contrast

--- lib/highlighter.rb
+++ lib/highlighter.rb
@@ -1,5 +1,6 @@
 require "redcarpet"
 require "coderay"
+require "nokogiri"
 
 module Highlighter
   class MissingLanguageError < StandardError; end
@@ -17,42 +18,68 @@ module Highlighter
 
       raise MissingLanguageError, error_message if language.nil?
 
-      language, file_name = _detect_language_and_filename(language)
+      language, file_name, changes = _detect_language_filename_and_changes(language)
 
       result = %Q{<div class="highlight #{language} #{class_name}">}
       result += '<div class="ribbon"></div>'
       result += '<div class="scroller">'
-      result += _code_table(string, language, file_name)
+      result += _code_table(string, language, file_name, changes)
       result += '</div>'
       result += %Q{</div>}
       result
     end
 
-    def _detect_language_and_filename(language)
-
+    def _detect_language_filename_and_changes(language)
       file_name = nil
+      changes = []
+      changes_regex = /\{(.+)\}$/
       bare_language_regex = /\A\w+\z/
+
+      if change_numbers = changes_regex.match(language)
+        language = language.sub(changes_regex, '')
+
+        changes = _parse_changes(change_numbers[1])
+      end
 
       unless language =~ bare_language_regex
         file_name = language
 
-        language = case /\.(\w+)$/.match(language)[1]
-                   when 'hbs'
-                     'handlebars'
-                   when 'js'
-                     'javascript'
-                   when 'html'
-                     'html'
-                   when 'css'
-                     'css'
-                   when 'json'
-                     'json'
-                   end
+        language = _determine_language(language)
       end
-      [language, file_name]
+      [language, file_name, changes]
     end
 
-    def _code_table(string, language, file_name)
+    def _determine_language(language)
+      case /\.(\w+)$/.match(language)[1]
+      when 'hbs'
+        'handlebars'
+      when 'js'
+        'javascript'
+      when 'html'
+        'html'
+      when 'css'
+        'css'
+      when 'json'
+        'json'
+      end
+    end
+
+    def _parse_changes(change_numbers)
+      changes = change_numbers.split(',').map do |change|
+        state = case change.slice(0)
+                when '+'
+                  'added'
+                when '-'
+                  'removed'
+                else
+                  nil
+                end
+
+        [change.to_i.abs, state]
+      end
+    end
+
+    def _code_table(string, language, file_name, changes)
       code = CodeRay.scan(string, language)
 
       table = code.div css: :class,
@@ -71,7 +98,30 @@ module Highlighter
 HEADER
       end
 
-      table
+      _highlight_lines(table, changes)
+    end
+
+    def _highlight_lines(table, highlights)
+      return table if highlights.empty?
+
+      table_xml = Nokogiri.XML(table)
+
+      numbers_node = table_xml.at_xpath('//td[@class="line-numbers"]/pre')
+      code_node = table_xml.at_xpath('//td[@class="code"]/pre')
+
+      numbers_contents = numbers_node.inner_html.split("\n")
+      code_contents = code_node.inner_html.split("\n")
+
+      highlights.each do |line_number, state|
+        index = line_number - 1
+        numbers_contents[index] = "<span class=\"highlight-line #{state}\">#{numbers_contents[index]}</span>"
+        code_contents[index] = "<span class=\"highlight-line #{state}\">#{code_contents[index]}</span>"
+      end
+
+      numbers_node.inner_html = numbers_contents.join("\n")
+      code_node.inner_html = code_contents.join("\n")
+
+      table_xml.to_xml
     end
 
     def highlight(language, class_name, &block)
