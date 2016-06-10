d52404ffdb0e0e83810ca851d584a042b7b5b9c5
David Baker <dbaker@acorncomputersolutions.com>
2016-06-09 21:26:34 -0600

    Setup continuous delivery to staging

---
+++ .travis/continuous-delivery.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+REPO="https://github.com/emberjs/guides.emberjs.com.git"
+BUILT_FILES=$HOME/build/emberjs/guides/build
+DEPLOY=$HOME/deploy
+WEBSITE_FILES=$DEPLOY/guides.emberjs.com
+
+# setup a temp folder to run things out of
+mkdir $DEPLOY
+cd $DEPLOY
+
+echo "Installing Firebase tools for deploys"
+npm install firebase-tools@^2.1 -g
+
+git clone $REPO
+
+# get latest version so we can copy our files to the right snapshot dir
+cd guides.emberjs.com/snapshots
+a=(*/)
+tmp=${a[@]: -1} # has a trailing slash we remove below
+latestVersion=${tmp%/}
+
+LATEST_VERSION=$WEBSITE_FILES/snapshots/$latestVersion
+
+cd $DEPLOY
+
+rm -rf $LATEST_VERSION
+cp -r $BUILT_FILES $LATEST_VERSION
+
+# start the deploy
+cd $WEBSITE_FILES
+firebase deploy -f ember-guides-staging
